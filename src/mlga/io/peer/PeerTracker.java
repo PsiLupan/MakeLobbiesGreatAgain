41d0851db7f835423f0abcc7220a857066971d9c
Will C <wcarter312@gmail.com>
2017-05-26 19:06:49 -0500

    Moved to Java8 Stream of forEachOrdered

--- src/mlga/io/peer/PeerTracker.java
+++ src/mlga/io/peer/PeerTracker.java
@@ -1,12 +1,13 @@
 package mlga.io.peer;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 /**
  * Incredibly grimy, unfinished (!) test class for parsing MLGA log files for (background) pairing of UID:IP to enable persistant blocks past dynamic IP ranges.
@@ -16,11 +17,15 @@ import java.util.ArrayList;
 public class PeerTracker {
 	static File dir = new File(new File(System.getenv("APPDATA")).getParentFile().getAbsolutePath()+"/Local/DeadByDaylight/Saved/Logs/");
 	static ArrayList<IOPeer> users = new ArrayList<IOPeer>();
+	static String uid = null;
+	static boolean active = false;
 	
 	public static void main(String[] args){
 		for(File f : dir.listFiles()){
-			if(f.isDirectory())continue;
-			if(!f.getName().endsWith(".log"))continue;
+			if(f.isDirectory())
+				continue;
+			if(!f.getName().endsWith(".log"))
+				continue;
 			System.out.println(f.getName());
 			processLog(f);
 		}
@@ -37,53 +42,54 @@ public class PeerTracker {
 	 * Otherwise, if the Peer ID is set & matches, and the IP isn't saved in its list, add the IP and save.
 	 * If none of the IOPeers trigger a match, build new IOPeer object, set name/IP, and save.
 	 * 	++There really *should* be a matching peer for the IP by this point, as the peer should've been created for the IP when first joined to.
-	*/
+	 */
 	private static void processLog(File f){
-		
-		try {
-			BufferedReader log = new BufferedReader(new FileReader(f));
-			String l;
+		try (Stream<String> stream = Files.lines(Paths.get(f.getPath()))) {			
 			
-			String uid = null;
-			boolean active = false;
-			
-			while((l = log.readLine())!=null){
+			stream.forEachOrdered((l)->{				
 				l = l.trim().toLowerCase();
-				
-				if(l.contains("connectionactive: 1"))active = true;
-				if(l.contains("connectionactive: 0"))active = false;
+
+				if(l.contains("connectionactive: 1"))
+					active = true;
+				if(l.contains("connectionactive: 0"))
+					active = false;
 				if(!active){
 					uid = null;
-					continue;
+					return;
 				}
 				if(l.contains("steam: - id:")){
 					uid = l.split("id:")[1].split("\\[")[1].split("\\]")[0].trim();
 				}
 				if(l.contains("-- ipaddress:")){
 					String ip = "";
-					if(uid!=null && active){
+					if(uid != null && active){
 						ip = l.split("address:")[1].trim();
 						if(ip.contains(":"))ip = ip.substring(0, ip.indexOf(":"));
-						InetAddress ina = InetAddress.getByName(ip);
-						if(ina.isAnyLocalAddress()){
-							uid = null;
+						InetAddress ina = null;
+						try {
+							ina = InetAddress.getByName(ip);
+							if(ina != null && ina.isAnyLocalAddress()){
+								uid = null;
+								active = false;
+								return;
+							}
+							IOPeer p = new IOPeer();
+							p.setUID(uid);
+							p.addIP(ip);
+							if(!users.contains(p)){
+								users.add(p);
+								System.out.println("\t"+uid+" = "+ip);
+							}
+						} catch (UnknownHostException e) {
+							e.printStackTrace();
+						}finally{
 							active = false;
-							continue;
+							uid = null;
 						}
-						IOPeer p = new IOPeer();
-						p.setUID(uid);
-						p.addIP(ip);
-						if(!users.contains(p)){
-							users.add(p);
-							System.out.println("\t"+uid+" = "+ip);
-						}
-						active = false;
-						uid = null;
 					}
-					
+
 				}
-			}
-			log.close();
+			});
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
